c2776424462315755883156ddeac1cc2e25b7448
Jose Naranjo <jnaranjo@clicklease.com>
2025-07-10 14:39:31 -0600

    added error handling in the UI

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ import { UserMessage, BotMessage } from './message.js';
 
 import { JokeController } from './joke-controller.js';
 
-
 async function tellAJoke() {
   ui.disableButton(ui.jokeButton, true);
   const userMessage = new UserMessage("Tell me a joke");
@@ -12,22 +11,22 @@ async function tellAJoke() {
   const waitMessage = new BotMessage("wait...");
   await ui.wait(800);
   ui.post(waitMessage.getHtml());
-  const joke = await comedian.getJoke();
-  const setup = new BotMessage(joke.setup);
-  const punchline = new BotMessage(joke.punchline);
-  await ui.wait(1000);
-  ui.post(setup.getHtml());
-  await ui.wait(2000);
-  ui.post(punchline.getHtml());
+  try {
+    const joke = await comedian.getJoke();
+    const setup = new BotMessage(joke.setup);
+    const punchline = new BotMessage(joke.punchline);
+    await ui.wait(1000);
+    ui.post(setup.getHtml());
+    await ui.wait(2000);
+    ui.post(punchline.getHtml());
+  } catch (error) {
+    const errorMessage = new BotMessage("Sorry, I couldn't fetch a joke right now. Please try again later.");
+    ui.post(errorMessage.getHtml());
+  }
   ui.disableButton(ui.jokeButton, false);
 }
-
 
 const ui = new UIController();
 const comedian = new JokeController("https://official-joke-api.appspot.com/jokes/random");
 ui.addClickHandler(ui.jokeButton, tellAJoke);
 ui.addClickHandler(ui.reloadButton, ui.reload);
-
-
-
-  
